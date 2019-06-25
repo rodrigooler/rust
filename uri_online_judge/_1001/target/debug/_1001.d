9b88a5fdff1128b0902c0da5fe1f288e18d6a61a
Oler <roodrigoprogrammer@gmail.com>
2019-06-25 00:34:11 -0300

    remove target

--- uri_online_judge/_1001/target/debug/_1001.d
+++
@@ -1,1 +0,0 @@
-/home/oler/www/rust/rust/uri_online_judge/_1001/target/debug/_1001: /home/oler/www/rust/rust/uri_online_judge/_1001/src/main.rs
--- uri_online_judge/_1001/target/debug/deps/_1001-de9a3e06f67401d6.d
+++
@@ -1,5 +0,0 @@
-/home/oler/www/rust/rust/uri_online_judge/_1001/target/debug/deps/_1001-de9a3e06f67401d6: src/main.rs
-
-/home/oler/www/rust/rust/uri_online_judge/_1001/target/debug/deps/_1001-de9a3e06f67401d6.d: src/main.rs
-
-src/main.rs:
