94cb326898b95ef9d23baf52c1b8a5f5d1703fa8
Oler <roodrigoprogrammer@gmail.com>
2019-06-25 00:33:42 -0300

    improve src/main.rs

--- uri_online_judge/_1001/src/main.rs
+++ uri_online_judge/_1001/src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+
+fn get_input() -> String {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    input
+}
+
+fn parse_i32(string: String) -> i32 {
+    string.trim().parse().unwrap_or(0)
+}
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = parse_i32(get_input());
+    let b = parse_i32(get_input());
+
+    println!("X = {}", add(a, b));
 }
---
+++ uri_online_judge/_1001/target/debug/_1001.d
@@ -0,0 +1,1 @@
+/home/oler/www/rust/rust/uri_online_judge/_1001/target/debug/_1001: /home/oler/www/rust/rust/uri_online_judge/_1001/src/main.rs
